98e2a6740834dbe2f57c6c0918220d02916658b9
Admin <admin@yext.com>
2016-05-11 15:58:26 -0400

    Add complete BeatBox project

---
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/Sound.java
@@ -0,0 +1,38 @@
+package com.bignerdranch.android.beatbox;
+
+/**
+ * Created by admin on 5/11/16.
+ */
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+    private Integer mSoundId;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        if (filename.contains("_")) {
+            components = filename.split("_");
+            filename = components[components.length - 1];
+        }
+        mName = filename.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
+    }
+}
